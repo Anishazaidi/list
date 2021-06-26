b205afc1bf25d5ce5c67a406cd100f46278f712a
Anishazaidi <anishazaidi1999@gmail.com>
2021-06-26 16:40:11 +0530

    first

---
+++ indexning.py
@@ -0,0 +1,31 @@
+
+# list1=[4,3,2,[4,5,[5,11,12],[1,2,[3,4],],],]                                                                                                                       
+# print(list1[3][2][2])
+# print(list1[3][3][2])
+# print(list1[3][1])
+# print(list[][])
+# str1="Anisha"
+# i=0
+# while i<len(str1):
+#     if str1[i]=="h":
+#         break
+#     else:
+#         print(str1[i])
+#     i=i+1
+# print("loop finish")    
+
+
+n=int(input("enter the num"))
+s=0
+while n>0:
+    r=n%10
+    s=s+r
+    n=n//10
+print(s)
+
+
+
+
+    
+
+
