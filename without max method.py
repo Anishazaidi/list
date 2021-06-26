3dd6c93d0fce93fc15eca070341d41c099b2007c
Anishazaidi <anishazaidi1999@gmail.com>
2021-06-26 16:59:56 +0530

    second commit

---
+++ without max method.py
@@ -0,0 +1,17 @@
+# list=[10,20,30,40]
+# i=0
+# max=0
+# while i<len(list):
+#     if list[i]>max:
+#         max=list[i]
+#     i=i+1
+# print(max)
+
+
+# list=[10,20,30,40]
+# print (max(list))
+
+
+list1=[4,3,2,[4,5,[5,11,12],[1,2[3,4]]]]  
+print([3][2][2])
+
